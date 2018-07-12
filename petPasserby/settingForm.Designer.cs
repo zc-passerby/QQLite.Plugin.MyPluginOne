1b9664d319c6431daf279077822581eda709f5ef
Passerby <450343225@qq.com>
2018-07-12 23:02:54 +0800

    1. 修改了控件名字，与类名冲突了 2. 增加了获取群列表功能---未完善

--- petPasserby/settingForm.Designer.cs
+++ petPasserby/settingForm.Designer.cs
@@ -32,12 +32,12 @@
             this.chClusterName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.chClusterSwitch = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.ClusterList = new System.Windows.Forms.TabPage();
+            this.tab_ClusterList = new System.Windows.Forms.TabPage();
             this.listView_clusterList = new System.Windows.Forms.ListView();
-            this.CmdSetting = new System.Windows.Forms.TabPage();
-            this.RespSetting = new System.Windows.Forms.TabPage();
+            this.tab_CmdSetting = new System.Windows.Forms.TabPage();
+            this.tab_RespSetting = new System.Windows.Forms.TabPage();
             this.tabControl1.SuspendLayout();
-            this.ClusterList.SuspendLayout();
+            this.tab_ClusterList.SuspendLayout();
             this.SuspendLayout();
             // 
             // chCulsterId
@@ -54,25 +54,25 @@
             // 
             // tabControl1
             // 
-            this.tabControl1.Controls.Add(this.ClusterList);
-            this.tabControl1.Controls.Add(this.CmdSetting);
-            this.tabControl1.Controls.Add(this.RespSetting);
+            this.tabControl1.Controls.Add(this.tab_ClusterList);
+            this.tabControl1.Controls.Add(this.tab_CmdSetting);
+            this.tabControl1.Controls.Add(this.tab_RespSetting);
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(578, 416);
             this.tabControl1.TabIndex = 0;
             // 
-            // ClusterList
+            // tab_ClusterList
             // 
-            this.ClusterList.Controls.Add(this.listView_clusterList);
-            this.ClusterList.Location = new System.Drawing.Point(4, 22);
-            this.ClusterList.Name = "ClusterList";
-            this.ClusterList.Padding = new System.Windows.Forms.Padding(3);
-            this.ClusterList.Size = new System.Drawing.Size(570, 390);
-            this.ClusterList.TabIndex = 0;
-            this.ClusterList.Text = "群列表";
-            this.ClusterList.UseVisualStyleBackColor = true;
+            this.tab_ClusterList.Controls.Add(this.listView_clusterList);
+            this.tab_ClusterList.Location = new System.Drawing.Point(4, 22);
+            this.tab_ClusterList.Name = "tab_ClusterList";
+            this.tab_ClusterList.Padding = new System.Windows.Forms.Padding(3);
+            this.tab_ClusterList.Size = new System.Drawing.Size(570, 390);
+            this.tab_ClusterList.TabIndex = 0;
+            this.tab_ClusterList.Text = "群列表";
+            this.tab_ClusterList.UseVisualStyleBackColor = true;
             // 
             // listView_clusterList
             // 
@@ -89,25 +89,26 @@
             this.listView_clusterList.TabIndex = 0;
             this.listView_clusterList.UseCompatibleStateImageBehavior = false;
             this.listView_clusterList.View = System.Windows.Forms.View.Details;
+            this.listView_clusterList.SelectedIndexChanged += new System.EventHandler(this.listView_clusterList_SelectedIndexChanged);
             // 
-            // CmdSetting
+            // tab_CmdSetting
             // 
-            this.CmdSetting.Location = new System.Drawing.Point(4, 22);
-            this.CmdSetting.Name = "CmdSetting";
-            this.CmdSetting.Padding = new System.Windows.Forms.Padding(3);
-            this.CmdSetting.Size = new System.Drawing.Size(570, 390);
-            this.CmdSetting.TabIndex = 1;
-            this.CmdSetting.Text = "命令语设置";
-            this.CmdSetting.UseVisualStyleBackColor = true;
+            this.tab_CmdSetting.Location = new System.Drawing.Point(4, 22);
+            this.tab_CmdSetting.Name = "tab_CmdSetting";
+            this.tab_CmdSetting.Padding = new System.Windows.Forms.Padding(3);
+            this.tab_CmdSetting.Size = new System.Drawing.Size(570, 390);
+            this.tab_CmdSetting.TabIndex = 1;
+            this.tab_CmdSetting.Text = "命令语设置";
+            this.tab_CmdSetting.UseVisualStyleBackColor = true;
             // 
-            // RespSetting
+            // tab_RespSetting
             // 
-            this.RespSetting.Location = new System.Drawing.Point(4, 22);
-            this.RespSetting.Name = "RespSetting";
-            this.RespSetting.Size = new System.Drawing.Size(570, 390);
-            this.RespSetting.TabIndex = 2;
-            this.RespSetting.Text = "回复语设置";
-            this.RespSetting.UseVisualStyleBackColor = true;
+            this.tab_RespSetting.Location = new System.Drawing.Point(4, 22);
+            this.tab_RespSetting.Name = "tab_RespSetting";
+            this.tab_RespSetting.Size = new System.Drawing.Size(570, 390);
+            this.tab_RespSetting.TabIndex = 2;
+            this.tab_RespSetting.Text = "回复语设置";
+            this.tab_RespSetting.UseVisualStyleBackColor = true;
             // 
             // settingForm
             // 
@@ -118,16 +119,16 @@
             this.Name = "settingForm";
             this.Text = "插件设置";
             this.tabControl1.ResumeLayout(false);
-            this.ClusterList.ResumeLayout(false);
+            this.tab_ClusterList.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.TabPage ClusterList;
-        private System.Windows.Forms.TabPage CmdSetting;
+        private System.Windows.Forms.TabPage tab_ClusterList;
+        private System.Windows.Forms.TabPage tab_CmdSetting;
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage RespSetting;
+        private System.Windows.Forms.TabPage tab_RespSetting;
         private System.Windows.Forms.ListView listView_clusterList;
         //ListView列名 begin
         private System.Windows.Forms.ColumnHeader chCulsterId;
--- petPasserby/settingForm.cs
+++ petPasserby/settingForm.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using QQLite.Framework.Entity;
+
 namespace petPasserby
 {
     public partial class settingForm : Form
@@ -17,6 +19,25 @@ namespace petPasserby
             InitializeComponent();
             this.Icon = QQLite.Framework.License.SoftIcon;
             this.Plugin = plugin;
+            initData();
+        }
+
+        private void initData()
+        {
+            //获取群列表
+            ClusterList clusterlist = Plugin.Client.ClusterList;
+            foreach(KeyValuePair<uint, ClusterInfo> kv in clusterlist)
+            {
+                ClusterInfo c = kv.Value;
+                ListViewItem lvi = listView_clusterList.Items.Add(c.ClusterId.ToString());
+                lvi.SubItems.Add(c.Name);
+                lvi.SubItems.Add("关");
+            }
+        }
+
+        private void listView_clusterList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
