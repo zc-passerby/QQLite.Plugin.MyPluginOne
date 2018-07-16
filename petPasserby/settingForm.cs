155eb01f60a49910f5045b1999ae23e97939bf5f
Passerby <450343225@qq.com>
2018-07-16 22:58:28 +0800

    增加了从数据库获取好友设置、默认群设置、群设置的开关

--- petPasserby/settingForm.cs
+++ petPasserby/settingForm.cs
@@ -26,22 +26,50 @@ namespace petPasserby
         {
             ListViewItem lvi;
             //先添加0（好友设置）和10000（默认群设置）
+            string SelectSql = "select IsEnabled from GroupConfig where GroupId='0';";
+            object friendSwitch = DbHelper.ExecuteScalar(SelectSql);
+            SelectSql = "select IsEnabled from GroupConfig where GroupId='10000';";
+            object defaultSwitch = DbHelper.ExecuteScalar(SelectSql);
             lvi = new ListViewItem("0");
             lvi.SubItems.Add("好友设置");
-            lvi.SubItems.Add("开");
+            if (friendSwitch != null && friendSwitch.ToString() == "0")
+            {
+                lvi.SubItems.Add("关");
+            }
+            else
+            {
+                lvi.SubItems.Add("开");
+            }
             lvi = listView_clusterList.Items.Add(lvi);
             lvi = new ListViewItem("10000");
             lvi.SubItems.Add("默认群设置");
-            lvi.SubItems.Add("开");
+            if(defaultSwitch != null && defaultSwitch.ToString() == "0")
+            {
+                lvi.SubItems.Add("关");
+            }
+            else
+            {
+                lvi.SubItems.Add("开");
+            }
             lvi = listView_clusterList.Items.Add(lvi);
             //获取群列表
             ClusterList clusterlist = Plugin.Client.ClusterList;
             foreach(KeyValuePair<uint, ClusterInfo> kv in clusterlist)
             {
-                ClusterInfo c = kv.Value;
-                lvi = new ListViewItem(c.ClusterId.ToString());
-                lvi.SubItems.Add(c.Name);
-                lvi.SubItems.Add("关");
+                ClusterInfo clusterInfo = kv.Value;
+                string clusterId = kv.Key.ToString();
+                SelectSql = "select IsEnabled from GroupConfig where GroupId='" + clusterId + "';";
+                object clusterSwitch = DbHelper.ExecuteScalar(SelectSql);
+                lvi = new ListViewItem(clusterId);
+                lvi.SubItems.Add(clusterInfo.Name);
+                if(null != clusterSwitch && clusterSwitch.ToString() == "1")
+                {
+                    lvi.SubItems.Add("开");
+                }
+                else
+                {
+                    lvi.SubItems.Add("关");
+                }
                 lvi = listView_clusterList.Items.Add(lvi);
             }
         }
