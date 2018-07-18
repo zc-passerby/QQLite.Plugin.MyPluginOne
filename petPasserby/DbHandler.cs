3b5e480177f69233f2ca35337395d50e3361d955
Passerby <450343225@qq.com>
2018-07-18 18:31:01 +0800

    增加数据库操作类，用于该项目中对数据库的增删改查的操作

---
+++ petPasserby/DbHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace petPasserby
+{
+    public class DbHandler
+    {
+        public static string getClusterIsEnabled(string clusterId)
+        {
+            string SelectSql = "select IsEnabled from GroupConfig where GroupId='{0}';";
+            SelectSql = string.Format(SelectSql, clusterId);
+            object isEnabled = DbHelper.ExecuteScalar(SelectSql);
+            if(isEnabled != null)
+            {
+                return isEnabled.ToString();
+            }
+            else
+            {
+                return "-1";
+            }
+
+        }
+    }
+}
