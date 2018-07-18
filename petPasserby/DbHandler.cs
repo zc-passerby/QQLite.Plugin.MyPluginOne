a07ad538a252a87203913d68e70581a404fb553f
Passerby <450343225@qq.com>
2018-07-18 22:50:51 +0800

    增加了函数

--- petPasserby/DbHandler.cs
+++ petPasserby/DbHandler.cs
@@ -7,20 +7,41 @@ namespace petPasserby
 {
     public class DbHandler
     {
-        public static string getClusterIsEnabled(string clusterId)
+        public static string getClusterIsEnabled(string GroupId)
         {
+            string retStr = null;
             string SelectSql = "select IsEnabled from GroupConfig where GroupId='{0}';";
-            SelectSql = string.Format(SelectSql, clusterId);
+            SelectSql = string.Format(SelectSql, GroupId);
             object isEnabled = DbHelper.ExecuteScalar(SelectSql);
             if(isEnabled != null)
             {
-                return isEnabled.ToString();
+                retStr = isEnabled.ToString();
             }
-            else
-            {
-                return "-1";
-            }
+            return retStr;
+        }
 
+        public static bool setClusterIsEnabled(string GroupId, int isEnabled)
+        {
+            bool bRet = false;
+            string updateSql = "update GroupConfig set IsEnabled={0} where GroupId='{1}';";
+            updateSql = string.Format(updateSql, isEnabled, GroupId);
+            if (DbHelper.ExecuteNonQuery(updateSql) == 1)
+            {
+                bRet = true;
+            }
+            return bRet;
+        }
+
+        public static bool addNewGroupInfo(string GroupId, int isEnabled)
+        {
+            bool bRet = false;
+            string insertSql = "insert into GroupConfig (GroupId,IsEnabled) values('{0}',{1});";
+            insertSql = string.Format(insertSql, GroupId, isEnabled);
+            if(DbHelper.ExecuteNonQuery(insertSql) == 1)
+            {
+                bRet = true;
+            }
+            return bRet;
         }
     }
 }
